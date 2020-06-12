4fdbe039b3c307f9aae0c29d3cad4af86d373271
Michal Terranova <MTerranova@talentpath.com>
2020-06-12 14:10:44 -0400

    completed quiz

--- authorquiz/src/AuthorQuiz.test.js
+++ authorquiz/src/AuthorQuiz.test.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import Adapter from 'enzyme-adapter-react-16';
+import AuthorQuiz from './AuthorQuiz';
+Enzyme.configure({ adapter : new Adapter() });
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+const state = {
+  turnData: {
+    books:['Hamlet', 'Othello', 'MacBeth', '1984', 'Animal Farm', 'The Road to Wigan Pier', 'Pride and Prejudice', 'Sense and Sensibility', 'Emma', 'Atlas Shrugged', 'Fountainhead', 'Anthem', 'The Dome', 'IT', 'The Shining', 'David Copperfield', 'A Tale of Two Cities', 'Great Expectations', 'The Adventures of Huckleberry Finn', 'Tom Sawyer', 'Life on the Mississippi' ], 
+    author : {
+      name : 'Charles Dickens',
+      imageUrl : 'image/authors/charlesdickens.jpg',
+      imageSource : 'Wikimedia Commons',
+      books : ['David Copperfield', 'A Tale of Two Cities']
+    },
+  },
+  highlight : 'none'
+}
+
+describe("Author Quiz", () => {
+  it("renders without crashing", () => {
+    const div = document.createElement("div");
+    ReactDOM.render(<AuthorQuiz {...state} onAnswerSelected={()=>{}} />, div);
+  });
+
+  describe("When no one has been selected", ()=>{
+    let wrapper;
+    beforeAll(()=> {
+      wrapper = mount(<AuthorQuiz {...state} onAnswerSelected={()=> {}} />
+      )});
+      
+      it("should have no background color", ()=> {
+        expect(wrapper.find("div.now.turn").props().style.backgroundColor).toBe('');
+      });
+    });
 });
--- authorquiz/src/index.js
+++ authorquiz/src/index.js
@@ -1,15 +1,113 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, withRouter} from 'react-router-dom'
 import './index.css';
 import AuthorQuiz from './AuthorQuiz';
 import * as serviceWorker from './serviceWorker';
+import {shuffle, sample} from 'underscore';
+import AddAuthorForm from './AddAuthorForm';
+const authors = [
+  {
+    name: 'Mark Twain',
+    imageUrl: "images/authors/marktwain.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['The Adventures of Huckleberry Finn', 'Tom Sawyer', 'Life on the Mississippi']
+  },
+  {
+    name: 'Charles Dickens',
+    imageUrl: "images/authors/charlesdickens.png",
+    imageSource: "Wikimedia Commons",
+    books: ['David Copperfield', 'A Tale of Two Cities', 'Great Expectations']
+  },
+  {
+    name: 'Stephen King',
+    imageUrl: "images/authors/stephenking.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['The Dome', 'IT', 'The Shining']
+  },
+  {
+    name: 'Ayn Rand',
+    imageUrl: "images/authors/aynrand.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['Atlas Shrugged', 'Fountainhead', 'Anthem']
+  },
+  {
+    name: 'Jane Austen',
+    imageUrl: "images/authors/janeausten.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['Pride and Prejudice', 'Sense and Sensibility', 'Emma']
+  },
+  {
+    name: 'George Orwell',
+    imageUrl: "images/authors/georgeorwell.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['1984', 'Animal Farm', 'The Road to Wigan Pier']
+  }, 
+  {
+    name: 'William Shakespeare',
+    imageUrl: "images/authors/williamshakespeare.jpg",
+    imageSource: "Wikimedia Commons",
+    books: ['Hamlet', 'Othello', 'MacBeth']
+  }
+];
+function getTurnData(authors) {
+  const allBooks = authors.reduce(function (p, c, i) {
+    return p.concat(c.books)
+  }, []);
+  const fourRandomBooks = shuffle(allBooks).slice(0,4);
+  const answer = sample(fourRandomBooks);
 
+  return {
+    books: fourRandomBooks, 
+    author : authors.find((author)=> 
+      author.books.some((title)=>
+      title === answer))
+  }
+}
+
+function resetState() {
+  return {
+    turnData: getTurnData(authors),
+    highlight: ''
+  }
+}
+
+let state = resetState();
+
+function onAnswerSelected(answer) {
+  const isCorrect = state.turnData.author.books.some((book) => book === answer);
+  state.highlight = isCorrect ? 'correct' : 'wrong';
+  render();
+}
+
+function App() {
+  return <AuthorQuiz {...state} 
+  onAnswerSelected = {onAnswerSelected}
+  onContinue={() =>{
+    state = resetState();
+    render();
+  }}/>
+}
+
+const AuthorWrapper = withRouter(({ history }) =>
+<AddAuthorForm onAddAuthor={author =>{
+    authors.push(author);
+    history.push('/')
+  }} />
+);
+function render(){
 ReactDOM.render(
-  <React.StrictMode>
-    <AuthorQuiz />
-  </React.StrictMode>,
+    <BrowserRouter>
+      <React.Fragment>
+        <Route exact path="/" component={App} />
+        <Route path="/add" component={AuthorWrapper} />
+      </React.Fragment>
+    </BrowserRouter>,
   document.getElementById('root')
 );
+}
+
+render();
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
