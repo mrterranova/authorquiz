99bf823d702020a54bdb560947404d5f17296458
Michal Terranova <MTerranova@talentpath.com>
2020-06-15 08:16:05 -0400

    Updates

---
+++ precache-manifest.2518ebb66b631d3ed2347849d766262d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "253e915a534de83f02863c1b21a24fea",
+    "url": "/authorquiz/index.html"
+  },
+  {
+    "revision": "e739f745d9f0d1955aec",
+    "url": "/authorquiz/static/css/main.a6fe319e.chunk.css"
+  },
+  {
+    "revision": "602882c87d271a1ca5c4",
+    "url": "/authorquiz/static/js/2.b8128578.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/authorquiz/static/js/2.b8128578.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e739f745d9f0d1955aec",
+    "url": "/authorquiz/static/js/main.f9891f38.chunk.js"
+  },
+  {
+    "revision": "8904c8fa7ff4f3263aef",
+    "url": "/authorquiz/static/js/runtime-main.2de16fd2.js"
+  }
+]);
